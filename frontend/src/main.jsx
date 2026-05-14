e98c375a9eb5f0b342d680857dd19c644c58d1ed
IT24100196 <IT24100196@my.sliit.lk>
2026-05-14 22:43:00 +0530

    Move frontend deploy settings to env vars

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,8 +3,12 @@ import { GoogleOAuthProvider } from '@react-oauth/google'
 import App from './App.jsx'
 import './index.css'
 
+const GOOGLE_CLIENT_ID =
+  import.meta.env.VITE_GOOGLE_CLIENT_ID ||
+  '136129495804-ce890rf6o3c59u3hmjfhhlobig6dsal6.apps.googleusercontent.com'
+
 createRoot(document.getElementById('root')).render(
-  <GoogleOAuthProvider clientId="136129495804-ce890rf6o3c59u3hmjfhhlobig6dsal6.apps.googleusercontent.com">
+  <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
     <App />
   </GoogleOAuthProvider>
 )
